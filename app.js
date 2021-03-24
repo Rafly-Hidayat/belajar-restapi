4423c49b397f4add49b7f27670fbfb6f4f7c6546
Rafly-Hidayat <rafly.hidayat2710@gmail.com>
2021-03-24 08:09:33 +0700

    rename beberapa file dan isi router agar lebih relevan

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const routerMhs = require('./routers/router')
+const routerMhs = require('./routers/mahasiswa')
 const app = express()
 const port = 3000
 
---
+++ routers/mahasiswa.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+const mhsController = require('../controller/mahasiswa')
+
+
+    router.get('/mahasiswa', mhsController.mhs)
+    
+    router.get('/mahasiswa/:id', mhsController.tampilmhsid)
+    
+    router.post('/tambah', mhsController.tambah)
+    
+    router.put('/ubah/:id', mhsController.ubah)
+    
+    router.delete('/hapus/:id', mhsController.hapus)
+
+    module.exports = router
